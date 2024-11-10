8cc93806a3302d0f91b41ec09c05de4ccc53ad67
digitaldnq <denqcr19@gmail.com>
2024-11-10 22:55:31 +0500

    Update homework9.py
    Исправил обработку

--- homework9/homework9.py
+++ homework9/homework9.py
@@ -1,7 +1,7 @@
 import re
 
 def processEmail(email):
-    pattern = r'^[a-zA-Z][\w.-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$'
+    pattern = r"^[^\d][a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 
     if not re.match(pattern, email):
         print("Некорректный формат email. Должно начинаться с буквы, содержать в себе знак - @")
